21104c210208bd26ea5678daefcfcda94a14a907
dbdimitrov <d.boykoff@gmail.com>
2021-03-09 18:10:58 +0100

    lig-rec interactions unfiltered

--- R/get_omnipath.R
+++ R/get_omnipath.R
@@ -1,28 +1,36 @@
-#' Function to call OmniPath resources
+#' Helper function that returns the name of each intercell resource in OmniPath
+#'
+#' @return a list of strings for each intercell resource in OmniPath
+get_lr_resources <- function(){
+    return(
+        list(
+            'CellChatDB',
+            'CellPhoneDB',
+            'Ramilowski2015',
+            'Baccin2019',
+            'LRdb',
+            'Kirouac2010',
+            'ICELLNET',
+            'iTALK',
+            'EMBRACE',
+            'HPMR',
+            'Guide2Pharma',
+            'connectomeDB2020',
+            'talklr',
+            'CellTalkDB',
+            'OmniPath'
+        )
+    )
+}
+
+
+
+#' Function to get intercell resources as in OmniPath
 #' @return A list with intercellular resources from OmniPath
+#' @import OmnipathR tidyverse
 get_omni_resources <- function(){
 
-    require(tidyverse)
-    require(OmnipathR)
-
-    omni_list <- list(
-        'CellChatDB',
-        'CellPhoneDB',
-        'Ramilowski2015',
-        'Baccin2019',
-        'LRdb',
-        'Kirouac2010',
-        'ICELLNET',
-        'iTALK',
-        'EMBRACE',
-        'HPMR',
-        'Guide2Pharma',
-        'connectomeDB2020',
-        'talklr',
-        'CellTalkDB',
-        'OmniPath'
-    )
-
+    omni_list <- get_lr_resources()
 
     # Need to come back and expand on categories
     # Replace ligand and receptor with more descriptive ones when available?
@@ -60,4 +68,216 @@ get_omni_resources <- function(){
 
     return(omni_resources)
 }
+
+
+#' Function to get unfiltered intercell resources
+#' For each resource and OmniPath variant compiles tables of ligands,
+#' receptors and interactions
+#' @details calls on omnipath_intercell, intercell_connections, get_partners,
+#' and intercell_connections
+#' @import magrittr tidyverse
+compile_ligrec <- function(){
+
+    # A list of OmniPath variants to be returned
+    omnipath_variants <- list(
+        # OmniPath_full = list(),
+        OmniPath_q50 = list(quality = .5),
+        OmniPath_ligrec = list(ligrec = TRUE),
+        OmniPath_ligrec_q50 = list(ligrec = TRUE, quality = .5)
+    )
+
+
+    get_lr_resources() %>%
+        map(function(resource){
+            list(ligands = get_ligands(resource),
+                 receptors = get_receptors(resource),
+                 connections = intercell_connections(resource))
+        }) %>%
+        setNames(get_lr_resources()) %>%
+        c(map(
+            omnipath_variants,
+            function(args){
+                args %<>% c(list(resource = 'OmniPath'))
+                list(ligands = do.call(get_ligands, args),
+                     receptors = do.call(get_receptors, args),
+                     connections = do.call(intercell_connections, args))
+            }
+        ))
+
+}
+
+
+#' Retrieves intercellular interactions from OmniPath
+#' @param quality
+#' @param ligrec
+#' @return A tibble with Intercell interactions from OmnIPath
+omnipath_intercell <- function(
+    quality = NULL,
+    ligrec = FALSE){
+
+    intracell <- c('intracellular_intercellular_related', 'intracellular')
+
+    transmitters <-
+        omnipath_partners(
+            side = 'ligand',
+            quality = quality,
+            ligrec = ligrec
+        ) %>%
+        filter(!parent %in% intracell) %>%
+        rename(category_source = source)
+
+    receivers <-
+        omnipath_partners(
+            side = 'receptor',
+            quality = quality,
+            ligrec = ligrec
+        ) %>%
+        filter(!parent %in% intracell) %>%
+        rename(category_source = source)
+
+    interactions <- import_post_translational_interactions()
+
+    interactions %>%
+        inner_join(
+            transmitters,
+            by = c('source' = 'uniprot')
+        ) %>%
+        group_by(
+            category, parent, source, target
+        ) %>%
+        mutate(
+            database = paste(database, collapse = ';')
+        ) %>%
+        summarize_all(first) %>%
+        inner_join(
+            receivers,
+            by = c('target' = 'uniprot'),
+            suffix = c('_intercell_source', '_intercell_target')
+        ) %>%
+        group_by(
+            category_intercell_source,
+            parent_intercell_source,
+            source,
+            target,
+            category_intercell_target,
+            parent_intercell_target
+        ) %>%
+        mutate(
+            database_intercell_target = paste(
+                database_intercell_target,
+                collapse = ';'
+            )
+        ) %>%
+        summarize_all(first) %>%
+        ungroup()
+
+}
+
+
+
+
+#' Retrieves the interactions from one ligand-receptor resource
+intercell_connections <- function(resource, ...){
+
+    if(resource == 'OmniPath'){
+
+        return(omnipath_intercell(...))
+
+    }
+
+    import_post_translational_interactions(
+        resource = resource,
+        entity_type = 'protein',
+        ...
+    ) %>%
+        as_tibble()
+
+}
+
+
+#' Retrieves ligands from one ligand receptor resource
+get_ligands <- function(resource, ...){
+
+    get_partners(side = 'ligand', resource = resource, ...)
+
+}
+
+
+#' Retrieves receptors from one ligand-receptor resource
+get_receptors <- function(resource, ...){
+
+    get_partners(side = 'receptor', resource = resource, ...)
+
+}
+
+
+
+#' Retrieves intercellular communication partners (ligands or receptors) from
+#' one ligand-receptor resource.
+get_partners <- function(side, resource, ...){
+
+    if(resource == 'OmniPath'){
+
+        return(omnipath_partners(side = side, ...))
+
+    }
+
+    id_cols <- `if`(
+        side == 'ligand',
+        syms(c('source', 'source_genesymbol')),
+        syms(c('target', 'target_genesymbol'))
+    )
+    up_col <- `if`(
+        side == 'ligand',
+        sym('source'),
+        sym('target')
+    )
+    gs_col <- `if`(
+        side == 'ligand',
+        sym('source_genesymbol'),
+        sym('target_genesymbol')
+    )
+
+    intercell_connections(resource, ...) %>%
+        select(!!!id_cols) %>%
+        distinct() %>%
+        rename(uniprot = !!up_col, genesymbol = !!gs_col)
+
+}
+
+
+
+
+#' Retrieves intercellular communication partners (transmitters or receivers)
+#' from OmniPath
+omnipath_partners <- function(side,
+                              quality = NULL,
+                              ligrec = FALSE){
+
+    causality <- list(ligand = 'trans', receptor = 'rec')
+
+    import_omnipath_intercell(
+        causality = causality[[side]],
+        scope = 'generic',
+        source = 'composite',
+        entity_type = 'protein') %>%
+        as_tibble() %>%
+        {`if`(
+            is.null(quality),
+            .,
+            group_by(., parent) %>%
+                filter(
+                    consensus_score >=
+                        quantile(consensus_score, quality)
+                ) %>%
+                ungroup()
+        )} %>%
+        {`if`(
+            ligrec,
+            filter(., parent == side),
+            .
+        )}
+}
+
+
 
