6b11027b6abf635466477d3cb42c3339f174274f
dbdimitrov <d.boykoff@gmail.com>
2021-06-08 10:21:56 +0200

    add natmi

--- analysis_scripts/test/unit_test_tobe.R
+++ analysis_scripts/test/unit_test_tobe.R
@@ -3,23 +3,25 @@ omni_resources <- readRDS("input/omni_resources.rds")
 op_resources <- list("CellChatDB" = omni_resources$CellChatDB)
 op_resources$CellChatDB %<>% filter(str_detect(target_genesymbol, "_"))
 
-seurat_object <- SeuratData::LoadData("pbmc3k")
-seurat_object@meta.data <- seurat_object@meta.data %>%
-    filter(seurat_annotations %in% c("NK", "CD8 T", "B")) %>%
-    rownames_to_column("id") %>%
-    group_by(seurat_annotations) %>%
-    top_n(30, nCount_RNA) %>%
-    as.data.frame() %>%
-    column_to_rownames("id")
+# seurat_object <- SeuratData::LoadData("pbmc3k")
+# seurat_object@meta.data <- seurat_object@meta.data %>%
+#     filter(seurat_annotations %in% c("NK", "CD8 T", "B")) %>%
+#     rownames_to_column("id") %>%
+#     group_by(seurat_annotations) %>%
+#     top_n(30, nCount_RNA) %>%
+#     as.data.frame() %>%
+#     column_to_rownames("id")
+#
+# seurat_object <- Seurat::CreateSeuratObject(counts = seurat_object@assays$RNA@counts,
+#                                             meta.data = seurat_object@meta.data)
+# seurat_object@meta.data %<>% na.omit()
+# seurat_object@meta.data$seurat_annotations %<>% factor()
+# seurat_object <- subset(seurat_object, cells = rownames(seurat_object@meta.data))
+# seurat_object <- SetIdent(seurat_object, value = seurat_object@meta.data$seurat_annotations)
+# seurat_object %<>% FindVariableFeatures()
+# saveRDS(seurat_object, "input/testdata.rds")
 
-seurat_object <- Seurat::CreateSeuratObject(counts = seurat_object@assays$RNA@counts,
-                                            meta.data = seurat_object@meta.data)
-seurat_object@meta.data %<>% na.omit()
-seurat_object@meta.data$seurat_annotations %<>% factor()
-seurat_object <- subset(seurat_object, cells = rownames(seurat_object@meta.data))
-seurat_object <- SetIdent(seurat_object, value = seurat_object@meta.data$seurat_annotations)
-seurat_object %<>% FindVariableFeatures()
-
+seurat_object <- readRDS("input/testdata.rds")
 
 # CellChat
 cc_res <- call_cellchat(op_resource = NULL,
@@ -71,3 +73,14 @@ squidpy_res <- call_squidpyR(seurat_object = seurat_object,
 
 
 # NATMI
+natmi_results <- call_natmi(omni_resources = omni_resources,
+                            seurat_object = seurat_object,
+                            omnidbs_path = "~/Repos/ligrec_decoupleR/input/omnipath_NATMI",
+                            natmi_path = "~/Repos/NATMI",
+                            em_path = "~/Repos/ligrec_decoupleR/input/test_em.csv",
+                            ann_path = "~/Repos/ligrec_decoupleR/input/test_ann.csv",
+                            output_path = "~/Repos/ligrec_decoupleR/output/test/natmi_run",
+                            .write_data = TRUE,
+                            .subsampling_pipe = FALSE,
+                            .assay = "SCT"
+)
