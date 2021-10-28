3e4e352df699c3c1d60503752f09fe4542b93d5c
dbdimitrov <d.boykoff@gmail.com>
2021-10-28 15:16:06 +0200

    test cellcall

--- tests/cellcall_test.R
+++ tests/cellcall_test.R
@@ -71,19 +71,134 @@ mt <- TransCommuProfile(object = mt,
 saveRDS(mt, "~/Downloads/cellcall_out.RDS")
 
 # from CellCall object to Seurat
-seurat_test <- CreateSeuratObject(CreateAssayObject(counts=mt@data$count))
-seurat_test <- AddMetaData(seurat_test, mt@meta.data)
+cellcall_out <- readRDS("~/Downloads/cellcall_out.RDS")
+gene_means <- cellcall_out@data$expr_mean %>% as_tibble(rownames="gene")
+
+# Start of Function
+my_Expr <- cellcall_out@data$withoutlog
+colnames(my_Expr) <- as.character(cellcall_out@meta.data$celltype)
+my_Expr[1:4,1:4]
+detect_gene <- rownames(my_Expr)
+
+
+
+#
+object = cellcall_out
+pValueCor=0.05
+CorValue=0.1
+topTargetCor=1
+method="weighted"
+p.adjust=0.05
+use.type="median"
+probs = 0.9
+Org = 'Homo sapiens'
+IS_core = TRUE
+
+# Read LR-TF-Pathway df
+f.tmp <- system.file("extdata", "new_ligand_receptor_TFs.txt", package="cellcall")
+triple_relation <- read.table(f.tmp, header = TRUE, quote = "", sep = '\t', stringsAsFactors=FALSE)
+f.tmp <- system.file("extdata", "tf_target_homology.txt", package="cellcall")
+triple_relation
+triple_relation$pathway_ID <- NULL
+print(triple_relation[1:4,])
+
+# TF-regulons
+target_relation <- read.table(f.tmp, header = TRUE, quote = "", sep = '\t', stringsAsFactors=FALSE)
+tmp_complex_symbol <- triple_relation$Receptor_Symbol[grep(",",triple_relation$Receptor_Symbol)] %>% unique() %>% str_split(",") %>% unlist %>% unique()
+
+all.gene.needed <- unique(as.character(c(triple_relation$Ligand_Symbol, triple_relation$Receptor_Symbol, triple_relation$TF_Symbol, target_relation$TF_Symbol, target_relation$Target_Symbol,tmp_complex_symbol)))
+# triple_relation[1:4,1:4]
+# target_relation[1:4,1:4]
+my_Expr <- object@data$withoutlog
+colnames(my_Expr) <- as.character(object@meta.data$celltype)
+my_Expr[1:4,1:4]
+
+
+
+expr_set <- my_Expr[intersect(detect_gene, all.gene.needed),]
+detect_gene <- rownames(expr_set)
+cell_type = unique(colnames(expr_set))
+expr.fc <- object@data$withoutlog[detect_gene,]
+colnames(expr.fc) <- colnames(expr_set)
+
+rm(list=c("object"))
+
+complex_matrix <- matrix(ncol = length(colnames(expr_set)))
+complex_matrix <- as.data.frame(complex_matrix)
+colnames(complex_matrix) <- colnames(expr_set)
+myrownames <- c()
+
+expr_set <- expr_set[apply(expr_set, 1, function(x){sum(x!=0)})>0,]
+detect_gene <- rownames(expr_set)
+# expr_set[1:4,1:4]
+
+print("step1: compute means of gene")
+expr_mean <- matrix(nrow = nrow(expr_set), ncol = length(cell_type))
+myColnames <- c()
+for (i in 1:length(cell_type)) {
+    myCell <- cell_type[i]
+    myMatrix <- expr_set[,colnames(expr_set)==myCell,drop=F]
+    if(use.type=="mean"){
+        myMatrix_mean <- as.numeric(apply(myMatrix, 1, mean))
+    }else if(use.type=="median"){
+        quantil.tmp <- as.numeric(apply(myMatrix, 1, function(x){
+            quantile(x, probs = probs,names=FALSE)
+        }))
+        mean.tmp <- rowMeans(myMatrix)
+        mean.tmp[which(quantil.tmp==0)]<-0
+        myMatrix_mean <- mean.tmp
+    }
+    expr_mean[,i] <- myMatrix_mean
+    myColnames <- c(myColnames, myCell)
+    # print(myCell)
+}
+expr_mean <- data.frame(expr_mean)
+colnames(expr_mean) <- myColnames
+rownames(expr_mean) <- rownames(expr_set)
+
+expr_mean <- expr_mean[apply(expr_mean, 1, function(x){sum(x!=0)})>0,]
+detect_gene <- rownames(expr_mean)
+
+
+#
+ligand_symbol <- unique(triple_relation$Ligand_Symbol)
+softmax_ligand <- expr_mean[intersect(ligand_symbol, detect_gene),]
+colnames(softmax_ligand) <- colnames(expr_mean)
+rowCounts <- rowSums(softmax_ligand)
+
+softmax_ligand <- do.call(rbind,lapply(1:nrow(softmax_ligand), function(i){
+    softmax_ligand[i,]/rowCounts[i]
+}))
+
+
+# Notes:
+# CellCall does not use the softmax function :D, it simply divides by the rowSums
+# i.e. each ligand is divided by the max of the same ligand.
+
+
+
+# Create seurat - TEST IN LIANA ----------
+seurat_test <- CreateSeuratObject(CreateAssayObject(counts=cellcall_out@data$count)) %>%
+    AddMetaData(cellcall_out@meta.data)
 Idents(seurat_test) <- seurat_test@meta.data$celltype
 
 #
 liana_res <- liana_pipe(seurat_test,
                         op_resource = select_resource("OmniPath")[[1]] %>%
                             decomplexify())
+
 liana_res %<>%
-    mutate(ligand.softmax = (ligand.expr)/ligand.sum)
+    group_by(ligand) %>%
+    mutate(ligand.max = max(ligand.sum)) %>%
+    ungroup() %>%
+    mutate(ligand.softmax = ligand.expr/ligand.max) %>%
+    group_by(receptor) %>%
+    mutate(receptor.max = max(receptor.sum)) %>%
+    ungroup() %>%
+    mutate(receptor.softmax = receptor.expr/receptor.max) %>%
+    rowwise() %>%
+    mutate(cellcall.score = mean(c(ligand.softmax,receptor.softmax)))
 
-liana_res %>%
-    filter(source=="ST")
 
-cellcall_ligand <- mt@data$softmax_ligand
-cellcall_ligand %>% head()
+
+cellcall_lr <- cellcall_out@data$expr_l_r %>% as_tibble(rownames="lr")
